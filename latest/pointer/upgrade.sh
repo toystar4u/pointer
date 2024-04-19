3e42400c694cb6fc55ea97cfa3af6e5eb714e1b3
toystar4u <46470737+toystar4u@users.noreply.github.com>
2024-04-19 11:01:12 +0900

    Update upgrade.sh

--- latest/pointer/upgrade.sh
+++ latest/pointer/upgrade.sh
@@ -14,52 +14,33 @@ CVER="$(cat version.txt.tmp)"
 
 if [ -z $CVER ];
 then
-	echo "Version Check Failed :: check the network connection"
+	echo "[UPGRADE] Version Check Failed :: check the network connection"
 	exit 0
 fi
 
 
 if [ -z $PVER ] || (( $(echo "$CVER > $PVER" |bc -l) ));
 then
-	echo "New version available: $CVER"
+	echo "[UPGRADE] New version available: $CVER"
 	mv version.txt.tmp version.txt
 
+	SWLIST="ppprtk_run ssr2osr_run ssr_test ssr_test_debug mbcssr_test mbcssr_test_debug"
 
-	## backup
-	echo "==> clbackupeanup...."
-	mv $TARGET/ppprtk_run $TARGET/ppprtk_run.bak
-	mv $TARGET/ssr2osr_run $TARGET/ssr2osr_run.bak
-	mv $TARGET/ssr_test $TARGET/ssr_test.bak
-	mv $TARGET/ssr_test_debug $TARGET/ssr_test_debug.bak
-	mv $TARGET/mbcssr_test $TARGET/mbcssr_test.bak
-	mv $TARGET/mbcssr_test_debug $TARGET/mbcssr_test_debug.bak
+	for SW in $SWLIST
+	do
+		echo "==> $SW backup...."
+		mv $TARGET/$SW $TARGET/$SW.bak
 
-	## downloading
-	echo "==> downloading"
-	wget ${REPO_URL}/ppprtk_run -P $TARGET
-	sudo chmod +x $TARGET/ppprtk_run
+		echo "==> $SW downloading"
+		wget ${REPO_URL}/$SW -P $TARGET
+		sudo chmod +x $TARGET/$SW
 
-	wget ${REPO_URL}/ssr2osr_run -P $TARGET
-	sudo chmod +x $TARGET/ssr2osr_run
-
-	wget ${REPO_URL}/ssr_test -P $TARGET
-	sudo chmod +x $TARGET/ssr_test
-
-	wget ${REPO_URL}/ssr_test_debug -P $TARGET
-	sudo chmod +x $TARGET/ssr_test_debug
-
-	wget ${REPO_URL}/mbcssr_test -P $TARGET
-	sudo chmod +x $TARGET/mbcssr_test
-
-	wget ${REPO_URL}/mbcssr_test_debug -P $TARGET
-	sudo chmod +x $TARGET/mbcssr_test_debug
-
-	## cleanup
-	echo "==> cleanup...."
-	#rm -rf $TARGET/ppprtk_run.* $TARGET/ssr2osr_run.* $TARGET/ssr_test.*
-	#rm -rf $TARGET/mbcssr_test.* $TARGET/mbcssr_test_debug.* $TARGET/ssr_test_debug.*
+		## cleanup
+		#echo "==> cleanup...."
+		#rm -rf $TARGET/S$W.*
+	done 
 else 
-	echo "The latest version $PVER used"
+	echo "[UPGRADE] The latest version is already installed: $PVER"
 
 	rm -rf ./version.txt.tmp
 fi
